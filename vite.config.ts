7b5d89bcb79ab92e7478eccc0e3e42868d213500
Mircea Casapu <mircea.casapu@gmail.com>
2023-09-02 21:28:45 +0300

    chore: add tsconfig relative imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@@routes": path.resolve(__dirname, "src/routes"),
+      "@@components": path.resolve(__dirname, "src/components"),
+    }
+  },
 })
