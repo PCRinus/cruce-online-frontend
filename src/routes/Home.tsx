e2f300d8232357618b58065315c70180ee928452
Mircea Casapu <mircea.casapu@gmail.com>
2023-09-02 23:41:34 +0300

    feat: add room component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -7,8 +7,11 @@ const Home: FC = () => {
     <>
       <h1 className="text-center text-5xl">Cruce online</h1>
 
-      <div className='my-4 flex flex-col gap-4'>
-        <Link to={`rooms`} className="rounded-md bg-green-700 px-4 py-1 text-center">
+      <div className="my-4 flex flex-col gap-4">
+        <Link
+          to={`rooms`}
+          className="rounded-md bg-green-700 px-4 py-1 text-center"
+        >
           Joaca acum
         </Link>
         <Button className="rounded-md bg-gray-400 px-4 py-1">
--- src/routes/Rooms.tsx
+++ src/routes/Rooms.tsx
@@ -1,9 +1,12 @@
+import Room from "@@components/Room";
 import { FC } from "react";
 
 const Rooms: FC = () => {
   return (
     <>
       <h1 className="text-center text-5xl">Camere</h1>
+
+      <Room />
     </>
   )
 };
